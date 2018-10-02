1965e28f8b3bdee903813ef596148e120baa76f0
Iván Moreno <contacto@ivanmoreno.me>
2018-10-02 15:16:21 +0200

    Test BBVA token

--- controllers/login.js
+++ controllers/login.js
@@ -1,16 +1,14 @@
 const shortid = require('shortid')
 const Client = require('../models/Client')
-const bbva = require('')
 
 exports.loginPage = (req, res) => {
     /* codificacion a base64 appID:OAUTH
     Buffer.from("Hello World").toString('base64')*/
-
-    res.render('login',{
-        code: req.query.code
-    })
+    res.render('login')
 }
 
 exports.bbvaToken = (req, res) => {
-    console.log(res.body)
+    res.render('login',{
+        code: req.query.code
+    })
 }
