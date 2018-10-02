b9d2ef6b8b6f520c3a4d4fd2b1c5446a5f2bfecf
Iván Moreno <contacto@ivanmoreno.me>
2018-10-02 12:26:35 +0200

    Directorios, setup inicial

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,3 @@
 window.jQuery = window.$ = require('./../../node_modules/jquery');
 require('../../node_modules/bootstrap')
 require('../scss/custom.scss')
-
-document.addEventListener('DOMContentLoaded', function (ev) {
-  window.onpopstate = function (ev) {
-    window.location.reload(true)
-  }
-  var stripe = Stripe('pk_live_xi7g8sZDZAxUQ73frozye1Ie');
-  var elements = stripe.elements();
-  var card = elements.create('cardNumber');
-  var exp = elements.create('cardExpiry');
-  var cvc = elements.create('cardCvc');
-  card.mount('#numero-tarjeta');
-  exp.mount('#caducidad-tarjeta');
-  cvc.mount('#cvv');
-})
