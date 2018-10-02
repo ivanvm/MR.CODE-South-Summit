adc22f628279ee55b442277f9b582f5848d3c6dd
Iván Vicente Moreno Cencerrado <contacto@ivanmoreno.me>
2018-10-03 00:41:18 +0200

    Continuación estructura

--- controllers/main.js
+++ controllers/main.js
@@ -5,3 +5,23 @@ exports.welcome = (req, res) => {
 exports.destination = (req, res) => {
     res.render('destination')
 }
+
+exports.date = (req, res) => {
+    res.render('destination')
+}
+
+exports.budget = (req, res) => {
+    res.render('destination')
+}
+
+exports.details = (req, res) => {
+    res.render('details')
+}
+
+exports.interests = (req, res) => {
+    res.render('interests')
+}
+
+exports.results = (req, res) => {
+    res.render('results')
+}
