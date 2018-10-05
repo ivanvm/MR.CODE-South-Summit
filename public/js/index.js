73fac35ecab6ce76e5683e4911c57d7e72fce939
Iván Moreno <contacto@ivanmoreno.me>
2018-10-05 03:38:58 +0200

    Final UI

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,4 @@
 window.jQuery = window.$ = require('./../../node_modules/jquery');
 require('../../node_modules/bootstrap')
 require('../scss/custom.scss')
+require('./../../node_modules/gsap/TweenMax')
